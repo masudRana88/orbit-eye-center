caf33898774b3e2e51f80b99ac30a43388de149b
masudRana88 <mahimuddin88@gmail.com>
2021-10-18 12:24:57 +0600

    home , Book Appoinntment add

--- src/components/page/Home/Appointment/Appointment.js
+++ src/components/page/Home/Appointment/Appointment.js
@@ -3,7 +3,10 @@ import './Appointment.css'
 const Appointment = () => {
     return (
         <div className="container-fluid appoinment-container mt-5">
-            <h1>hi</h1>
+            <div className="hero-contant">
+                <h2 className="font-oswald fw-bold fs-1"><span className="color-reguler">For A Clear</span> Tomorrow, Bring<br />Their World into Focus</h2>
+                <button className="btn bg-color-reguler appoinment-btn mt-3 text-light">Book Appointment</button>
+            </div>
         </div>
     );
 };
